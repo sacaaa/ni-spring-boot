527d7184b4381444935a6b3208c70a89a2e7c5e4
Sándor Székelyhidi <szekelyhidis@mailbox.unideb.hu>
2024-04-09 09:39:31 +0200

    edit: updated pom.xml with new dependency for runtime database set

---
+++ src/main/java/com/esen/bookstore/model/Book.java
@@ -0,0 +1,28 @@
+package com.esen.bookstore.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title, author, publisher;
+
+    private Double price;
+
+}
